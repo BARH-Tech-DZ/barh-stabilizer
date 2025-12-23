88f4f6f2e7d81b49d15f0b47a0c1549b86822435
BARH-Tech-DZ <nawat-inovation@hotmail.com>
2025-12-23 11:54:21 +0100

    Create barh.sh
    Create barh script

---
+++ barh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# BARH 0.786 - Official Script
+echo -e "\e[1;32m[+] BARH 0.786 Stabilizer is Running...\e[0m"
+sysctl -w net.ipv4.tcp_low_latency=1 > /dev/null 2>&1
+sync; echo 3 > /proc/sys/vm/drop_caches 2>/dev/null
+echo -e "\e[1;33m[✔] Optimization Complete. Power by BARH-Tech-DZ\e[0m"#!/bin/bash
+# BARH 0.786 - Official Script
+echo -e "\e[1;32m[+] BARH 0.786 Stabilizer is Running...\e[0m"
+sysctl -w net.ipv4.tcp_low_latency=1 > /dev/null 2>&1
+sync; echo 3 > /proc/sys/vm/drop_caches 2>/dev/null
+echo -e "\e[1;33m[✔] Optimization Complete. Power by BARH-Tech-DZ\e[0m"
